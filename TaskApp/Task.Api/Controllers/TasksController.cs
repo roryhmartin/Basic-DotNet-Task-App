c20ac44ce7b2dfa3dcda3b339e00f91cb9f50368
Rory Martin <rory.martin@mhr.co.uk>
2024-12-01 11:25:38 +0000

    Initial basic setup using CRUD. No databse

---
+++ TaskApp/Task.Api/Controllers/TasksController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Task.Api.Models;
+
+namespace Task.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        //In memory storage for simplicity
+        private static readonly List<TodoItem> _todoItems = [];
+
+        //GET: api/Tasks
+        [HttpGet]
+        public ActionResult<IEnumerable<TodoItem>> GetAllTasks()
+        {
+            return Ok(_todoItems);
+        }
+
+        //GET By ID: api/tasks/1
+        [HttpGet("{id}")]
+        public ActionResult<TodoItem> GetTaskById(int id)
+        {
+            var todoItem = _todoItems.FirstOrDefault(x => x.Id == id);
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+            return Ok(todoItem);
+        }
+
+        //Post: api/tasks
+        [HttpPost]
+        public ActionResult Post([FromBody] TodoItem todoItem)
+        {
+            _todoItems.Add(todoItem);
+            return CreatedAtAction(nameof(GetTaskById), new { id = todoItem.Id }, todoItem);
+        }
+
+    }
+}
---
+++ TaskApp/Task.Api/Models/TodoItem.cs
@@ -0,0 +1,12 @@
+﻿namespace Task.Api.Models;
+
+public class TodoItem
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Descripition { get; set; }
+
+    public bool IsComplete { get; set; }
+}
